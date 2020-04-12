99d93ce4b709dfb44c9a7f39b442d06456aa230c
Lucas Ferreira <ferreira.alves@live.com>
2020-04-12 12:01:06 -0300

    Database migration

--- corona/Data/Migrations/20200412145659_added-coutry-and-infection-data.Designer.cs
+++ corona/Data/Migrations/20200412145659_added-coutry-and-infection-data.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using corona.Data;
 
 namespace corona.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200412145659_added-coutry-and-infection-data")]
+    partial class addedcoutryandinfectiondata
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ corona/Data/Migrations/20200412145659_added-coutry-and-infection-data.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace corona.Data.Migrations
+{
+    public partial class addedcoutryandinfectiondata : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InfectionData",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ConfirmedCases = table.Column<int>(nullable: false),
+                    Deaths = table.Column<int>(nullable: false),
+                    Recovered = table.Column<int>(nullable: false),
+                    CountryId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InfectionData", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_InfectionData_Countries_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Countries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InfectionData_CountryId",
+                table: "InfectionData",
+                column: "CountryId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InfectionData");
+
+            migrationBuilder.DropTable(
+                name: "Countries");
+        }
+    }
+}
